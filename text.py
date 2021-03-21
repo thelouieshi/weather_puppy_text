a729baff977bbbf15c238d5cba478681204899d3
Louie Shi <61924766+thlouieshi@users.noreply.github.com>
2021-03-21 13:57:43 -0600

    Update text.py

--- text.py
+++ text.py
@@ -1,18 +1,18 @@
-from twilio.rest import Client
+from twilio.rest import Client  # You will need to install this package.
 
 class Text:
     def __init__(self):
-        self.weather_message = "test"
-        self.account_sid = "ACb1fabd138d7f5575bb001c4712bd4f1d"
-        self.auth_token = "e119420d47ae634aa954e21e850e0974"
+        self.weather_message = "place holder"  # it can be anything, because text.weather_message will be changed in the main.py
+        self.account_sid = "YOUR TWILIO ID"
+        self.auth_token = "YOUR TWILIO TOKEN"
 
     def weather_text(self):
         client = Client(self.account_sid, self.auth_token)
         message = client.messages \
             .create(
             body=self.weather_message,
-            from_='+15172012350',
-            to='+1YOUR NUMBER',
+            from_='+1 YOUR TWILIO NUMBER',  # the numbers will need to be this format: "+11234567890"
+            to='+1 YOUR NUMBER',
         )
 
         print(message.status)
@@ -22,8 +22,8 @@ class Text:
         message = client.messages \
             .create(
             body="Hang out with the dog!",
-            from_='+15172012350',
-            to='+1YOUR NUMBER',
+            from_='+1 YOUR TWILIO NUMBER',
+            to='+1 YOUR NUMBER',
         )
 
         print(message.status)
